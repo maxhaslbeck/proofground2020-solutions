fe9a4cd5b07aa07da599d00d857a4105a592da10
Dominique Larchey-Wendling <larchey@gmail.com>
2020-07-01 12:02:07 +0200

    improvements in the PHP and the riter lib

--- good_nodes/coq/dlw/Submission.v
+++ good_nodes/coq/dlw/Submission.v
@@ -1,5 +1,7 @@
 Require Import Defs Arith Lia Permutation.
 
+Infix "~p" := (@Permutation _) (at level 70).
+
 (** Small utility to be used in combination with the PHP 
     list_n n = [n-1;...;0] *)
 
@@ -26,42 +28,18 @@ Section php.
 
   Implicit Type (l : list X).
 
+  Fact in_Permutation x m : In x m -> exists l, x::l ~p m.
+  Proof.
+    intros H.
+    apply in_split in H as (l & r & ->).
+    exists (l++r); now apply Permutation_cons_app.
+  Qed.
+
   Fact incl_cons_inv_l x l m : incl (x::l) m <-> In x m /\ incl l m.
   Proof.
     split.
     + intros H; split; [ | intros ? ? ]; apply H; simpl; auto.
     + intros (? & ?) z [ -> | ? ]; auto.
-  Qed.
-
-  Definition has_dup l := exists x a b c, l = a++x::b++x::c.
-
-  Fact has_dup_cons x l : has_dup (x::l) <-> In x l \/ has_dup l.
-  Proof.
-    split.
-    * intros (y & [ | z a ] & b & c & H).
-      + inversion H; rewrite in_app_iff; simpl; auto.
-      + inversion H; right; exists y, a, b, c; auto.
-    * intros [ H | (y & a & b & c & ->) ].
-      + apply in_split in H; destruct H as (a & b & ->).
-        exists x, nil, a, b; auto.
-      + exists y, (x::a), b, c; auto. 
-  Qed.
-
-  Fact has_dup_perm l m : Permutation l m -> has_dup l -> has_dup m.
-  Proof.
-    induction 1; auto.
-    + rewrite !has_dup_cons.
-      intros [ H1 | ]; auto; left; revert H1.
-      apply Permutation_in; auto.
-    + rewrite !has_dup_cons; simpl.
-      intros [ [] | ? ]; auto; tauto.
-  Qed.
-
-  Fact has_dup_insert l x r : has_dup (l++r) -> has_dup (l++x::r).
-  Proof.
-    intros H; apply has_dup_perm with (l := x::l++r).
-    + apply Permutation_cons_app; auto.
-    + apply has_dup_cons; auto.
   Qed.
 
   Fact incl_cons_inv_r l x m : incl l (x::m) -> incl l m \/ In x l.
@@ -76,40 +54,59 @@ Section php.
         - right; simpl; auto.
   Qed.
 
+  Definition has_dup l := exists x a b c, l = a++x::b++x::c.
+
+  Fact has_dup_nil : has_dup nil <-> False.
+  Proof. split; try tauto; now intros (? & [] & ? & ? & ?). Qed.
+
+  Fact has_dup_cons x l : has_dup (x::l) <-> In x l \/ has_dup l.
+  Proof.
+    split.
+    * intros (y & [ | z a ] & b & c & H); inversion H.
+      + rewrite in_app_iff; simpl; auto.
+      + right; now exists y, a, b, c.
+    * intros [ H | (y & a & b & c & ->) ].
+      + apply in_split in H as (a & b & ->).
+        exists x, nil, a, b; auto.
+      + exists y, (x::a), b, c; auto. 
+  Qed.
+
+  Fact perm_has_dup l m : l ~p m -> has_dup l -> has_dup m.
+  Proof.
+    induction 1 as [ | x l m H IH | | ]; auto; rewrite !has_dup_cons; simpl.
+    + intros [ G | ]; auto; left; revert G; apply Permutation_in; auto.
+    + generalize (@eq_sym _ x y); tauto.
+  Qed.
+
   Fact incl_cons_inv_dup l x m : 
          incl l (x::m) 
       -> incl l m 
-      \/ (exists a b, l = a++x::b /\ incl (a++b) m) 
+      \/ (exists l', x::l' ~p l /\ incl l' m) 
       \/ has_dup l.
   Proof.
     intros H.
     destruct incl_cons_inv_r with (1 := H) as [ H1 | H1 ]; auto.
-    apply in_split in H1; destruct H1 as (a & b & ->).
-    destruct (incl_cons_inv_r a x m) as [ H2 | H2 ].
-    + intros ? ?; apply H, in_app_iff; auto.
-    + destruct (incl_cons_inv_r b x m) as [ H3 | H3 ].
-      * intros ? ?; apply H, in_app_iff; simpl; auto.
-      * right; left; exists a, b; split; auto.
-        intros ?; rewrite in_app_iff; intros []; auto.
-      * apply in_split in H3; destruct H3 as (c & d & ->).
-        do 2 right; exists x, a, c, d; auto.
-    + apply in_split in H2; destruct H2 as (c & d & ->).
-      do 2 right; exists x, c, d, b; rewrite app_ass; auto.
+    apply in_Permutation in H1 as (l' & H1).
+    destruct (incl_cons_inv_r l' x m) as [ H2 | H2 ]; auto.
+    + intros y Hy; apply H, Permutation_in with (1 := H1); right; auto.
+    + firstorder.
+    + apply in_split in H2 as (c & d & ->).
+      do 2 right; apply perm_has_dup with (1 := H1).
+      now exists x, nil, c, d.
   Qed.
 
   Theorem php l m : incl l m -> length m < length l -> has_dup l.
   Proof.
     revert l; induction m as [ | y m IHm ]; simpl; try lia; intros l;
       try (simpl; lia).
-    + destruct l as [ | x l ]; simpl; try lia. 
+    + destruct l as [ | x l ]; simpl; try lia.
       intros H; destruct (H x); simpl; auto.
     + intros H1 H2.
-      apply incl_cons_inv_dup in H1.
-      destruct H1 as [ H1 | [ (a & b & -> & H3) | H1 ] ]; auto.
+      apply incl_cons_inv_dup in H1 as [ H1 | [ (l' & H1 & H3) | H1 ] ]; auto.
       * apply IHm; auto; lia.
-      * rewrite app_length in H2; simpl in H2.
-        apply has_dup_insert, IHm; auto.
-        rewrite app_length; lia.
+      * apply perm_has_dup with (1 := H1), has_dup_cons; right.
+        apply Permutation_length in H1; simpl in H1.
+        apply IHm; auto; lia.
   Qed.
 
 End php.
@@ -215,14 +212,14 @@ Section rels.
       * intros (y & ? & ?); constructor 2 with y; auto.
   Qed.
 
-  Definition to_cycle x := exists y, x ->> y /\ y -+> y.
-
   (** If there is a chain of length k starting at x 
       and that chain is contained in a list of smaller length
-      then x leads to a cycle, ie x ~~> y ++> y 
+      then x leads to a cycle, ie x ~>> y -+> y 
 
       This involves the finite Pigeon Hole Principle 
-    *)  
+    *)
+
+  Definition to_cycle x := exists y, x ->> y /\ y -+> y.
 
   Fact riter_php l k x y : 
        (forall x y, R x y -> In y l)
@@ -270,6 +267,27 @@ Section rels.
     rewrite riter_plus.
     intros H1 (k & H2 & H3).
     now rewrite (riter_det _ _ _ _ H1 H2).
+  Qed.
+
+  Fact riter_prefix_choose a b x y z : 
+           x -[a]> y 
+        -> x -[b]> z 
+        -> y -[b-a]> z \/ z -[a-b]> y.
+  Proof.
+    intros H1 H2.
+    destruct (le_lt_dec a b).
+    + left; apply riter_plus_inv with (1 := H1).
+      now replace (a+(b-a)) with b by lia.
+    + right; apply riter_plus_inv with (1 := H2).
+      now replace (b+(a-b)) with a by lia.
+  Qed.
+
+  Fact riter_choose x y z : x ->> y -> x ->> z -> y ->> z \/ z ->> y.
+  Proof.
+    intros (a & Ha) (b & Hb).
+    destruct riter_prefix_choose with (1 := Ha) (2 := Hb).
+    + left; now exists (b-a).
+    + right; now exists (a-b).
   Qed.
 
   (** of x -+> .... ->> x is a cycle and x ->> y then also y ->> x 
@@ -345,6 +363,12 @@ Proof.
     contradict Hy; apply E_0_no_image.
 Qed.
 
+Fact rtc_E_from_0 i : 0 ->> i -> i = 0.
+Proof. intros (n & Hn); now apply riter_E_from_0 in Hn. Qed.
+
+Fact tc_E_from_0 i : 0 -+> i -> False.
+Proof. intros (n & Hn); now apply riter_E_from_0 in Hn. Qed.
+
 Hint Resolve E_0_no_image E_det : core.
 
 (** * task 2*)
@@ -363,31 +387,22 @@ Lemma good_node_characterisation_1 i:
   -> ~ exists j, clos_refl_trans_1n _ E i j
            /\ clos_trans_1n _ E j j.
 Proof.
-  intros H1 H2 (j & H3 & H4).
-  rewrite good_node_riter in H1.
-  rewrite riter_rtc in H3.
-  rewrite riter_tc in H4.
-  revert H1 H3 H4.
-  intros (a & Ha) (b & Hb) (c & Hc).
-  destruct (le_lt_dec b a).
-  + replace a with (b+(a-b)) in Ha by lia.
-    apply riter_plus_inv with (2 := Hb) in Ha; eauto.
-    destruct riter_cycle with (1 := E_det) (x := j) (y := 0)
-      as (k & Hk); eauto.
-    apply riter_E_from_0 in Hk.
-    destruct Hk; subst.
-    destruct Hc as (y & Hy & _).
-    revert Hy; apply E_0_no_image.
-  + replace b with (a+(b-a)) in Hb by lia.
-    apply riter_plus_inv with (2 := Ha) in Hb; eauto.
-    apply riter_E_from_0 in Hb; lia.
+  intros H1 H2 (j & H3 & H4); revert H1 H3 H4.
+  rewrite good_node_riter, riter_rtc, riter_tc; intros H1 H3 H4.
+  destruct riter_choose 
+    with (1 := E_det) (2 := H1) (3 := H3)
+    as [ C | C ].
+  + apply rtc_E_from_0 in C as ->.
+    apply tc_E_from_0 in H4 as [].
+  + apply riter_cycle with (1 := E_det) (2 := H4),
+          rtc_E_from_0 in C as ->.
+    apply tc_E_from_0 in H4 as [].
 Qed.
 
 (* Unless i ->> stops at 0, path from i can be arbitarily long *)
 
-Lemma path_stops_at_0_or_not i p : 
-    i < n -> i ->> 0
-          \/ (exists z, z < n /\ i -[p]> z).
+Lemma path_stops_at_0_or_as_long_as_wished i p : 
+    i < n -> i ->> 0 \/ exists z, z < n /\ i -[p]> z.
 Proof.
   intros Hi.
   induction p as [ | p [ H | (z & H1 & H2) ] ]; auto.
@@ -395,23 +410,24 @@ Proof.
   + destruct (eq_nat_dec z 0) as [ -> | C ].
     * left; exists p; auto.
     * right.
-      destruct E_total with z as (j & H); try lia.
-      exists j; split.
-      - apply H.
+      destruct E_total with z as (j & H3); try lia.
+      exists j; split; auto.
+      - apply H3.
       - rewrite riter_S; exists z; auto.
 Qed.
 
-(** Now with the PHP, we get a cycle out of a long path *)
+(** Now with the PHP, we get a cycle out of a long enough path *)
 
 Lemma stops_at_0_or_to_cycle i : 
    i < n -> i ->> 0 \/ to_cycle _ E i.
 Proof.
   intros Hi.
-  destruct (path_stops_at_0_or_not i (S n)) as [ H | (z & H1 & H2) ]; auto; right.
+  destruct (path_stops_at_0_or_as_long_as_wished i (S n))
+    as [ H | (z & H1 & H2) ]; auto; right.
   apply riter_php with (l := list_n n) (3 := H2).
   + intros ? ? []; rewrite list_n_spec; lia.
   + rewrite list_n_length; lia.
-Qed. 
+Qed.
 
 (** * task 3*)
 Lemma good_node_characterisation_2 i:
